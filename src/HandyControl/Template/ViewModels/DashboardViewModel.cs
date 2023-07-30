451c4c967c0f44dc2cd98b22655665fc984ca0d3
Memoyu <mmy6076@outlook.com>
2023-07-30 19:08:50 +0800

    feat: add log sample

--- src/HandyControl/Template/ViewModels/DashboardViewModel.cs
+++ src/HandyControl/Template/ViewModels/DashboardViewModel.cs
@@ -1,22 +1,35 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using HandyControl.Template.Controls;
-using HandyControl.Template.Views.Pages;
-using System.Collections.ObjectModel;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace HandyControl.Template.ViewModels;
 
 public partial class DashboardViewModel : ObservableObject, INavigationAware
 {
     private bool _isInitialized = false;
+    private readonly ILogger _logger;
 
-    public DashboardViewModel()
+    public DashboardViewModel(ILoggerFactory loggerFactory)
     {
-
+        _logger = loggerFactory.CreateLogger<DashboardViewModel>();
     }
 
     private void InitializeData()
     {
         _isInitialized = true;
+    }
+
+    [RelayCommand]
+    async Task WriteToLogClick()
+    {
+        _logger.LogDebug("写入Debug日志");
+        _logger.LogInformation("写入Information日志");
+        _logger.LogWarning("写入Warning日志");
+        _logger.LogError("写入Error日志");
+
+        await Task.CompletedTask;
     }
 
     public void OnNavigatedTo()
